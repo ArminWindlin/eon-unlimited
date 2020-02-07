e3af0961dc95c1c8f013a82ddb68c4ab55a4329c
ArminWindlin <armin.windlin@gmail.com>
2020-02-07 22:49:44 +0100

    added draggable module

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import './Board.scss';
 import Card from './Card';
+import Draggable from 'react-draggable';
 
 function Board() {
 
     const [count, setCount] = useState(0);
+    const [cardPosition, setCardPosition] = useState('relative');
     const [fruit] = useState('banana');
 
     useEffect(() => {
@@ -17,14 +19,37 @@ function Board() {
         setCount(42);
     }
 
+    function onCardDragStop(e: any, position: any) {
+        const {x, y} = position;
+        console.log(x);
+        console.log(y);
+    }
+
+    // @ts-ignore
     return (
         <div className="board">
+            <Draggable>
+                <div>This readme is really dragging on...</div>
+            </Draggable>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
             <p>I like to eat {fruit}</p>
-          <Card/>
+            <div className="card-container" id="card-container-1">
+                <Draggable onStop={onCardDragStop}>
+                    <div style={{position: 'relative'}}>
+                        <Card/>
+                    </div>
+                </Draggable>
+            </div>
+            <div className="card-container" id="card-container-2">
+                <Draggable>
+                    <div>
+                        <Card/>
+                    </div>
+                </Draggable>
+            </div>
         </div>
     );
 }
