5c205debd8f5773cd2ed12a1b7a43eda50af4ffd
ArminWindlin <armin.windlin@gmail.com>
2020-02-26 20:37:03 +0100

    added stats to cards

--- frontend/src/interfaces/CardType.ts
+++ frontend/src/interfaces/CardType.ts
@@ -1,10 +1,12 @@
 export default interface CardType {
     id: number,
     title: string,
+    // stats
     offense: number,
     defense: number,
     health: number,
     mana: number,
+    // usability
     selected: boolean,
     index: number,
     place: string,
