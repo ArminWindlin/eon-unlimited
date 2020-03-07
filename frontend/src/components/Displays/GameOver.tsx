3cf590b9ebe2cfe42f807bf28d98610f6eb57e74
ArminWindlin <armin.windlin@gmail.com>
2020-03-07 17:19:51 +0100

    Added game over condition and display

---
+++ frontend/src/components/Displays/GameOver.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Deck.scss';
+import {socket} from '../../utility/socket';
+
+const Deck: React.FC = () => {
+
+    const draw = () => {
+        socket.emit('ACTION_DRAW');
+    };
+
+    return (
+            <div className="deck" onClick={draw}>Draw</div>
+    );
+};
+
+export default Deck;
